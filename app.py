f0ac4fa8d3ba7b28132bc961fcdb8f9e9bd2b1a3
Kwan Jeon <jeonkwan100@gmail.com>
2024-02-29 09:47:38 +0900

    fix paginate

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 import random
 from flask import Flask, render_template, request, redirect, url_for
+from flask_paginate import Pagination, get_page_parameter
 
 app = Flask(__name__)
 
@@ -9,11 +10,24 @@ before_battle = []
 com_rsp_dic: dict = {1: 'r', 2: 's', 3: 'p'}
 
 
+def get_items(offset=0, per_page=10):
+    return all_history[offset: offset + per_page]
+
+
 @app.route('/')
 def home():
     count_list = {'win': before_battle.count('w'), 'lose': before_battle.count('l'), 'draw': before_battle.count('d')}
 
-    return render_template('index.html', all_history=all_history, count_list=count_list)
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+
+    per_page = 10
+    total = len(all_history)
+    pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
+
+    print(page)
+
+    items = get_items(offset=(page - 1) * per_page, per_page=per_page)
+    return render_template('index.html', count_list=count_list, items=items, pagination=pagination, per_page=per_page)
 
 
 @app.route('/cal_result/')
@@ -60,8 +74,7 @@ def cal_rsp():
 
     rsp_game(query)
 
-    print(f'승: {before_battle.count('w')} 패: {before_battle.count('l')} 무: {before_battle.count('d')}')
-
+    # print(f'승: {before_battle.count('w')} 패: {before_battle.count('l')} 무: {before_battle.count('d')}')
     return redirect(url_for('home'))
 
 
